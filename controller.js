bf6b884a0c97a0d4047d420cc1623484dc21340f
leduin <pepeleduin@duck.com>
2024-01-14 22:46:15 +0100

    Add layers and use them in cotroller

--- controller.js
+++ controller.js
@@ -1,39 +1,66 @@
 var map = L.map("map", { crs: L.CRS.EPSG4326 }).setView(
-    [40.40597439708923, -3.8355703741754645],
-    17
-  );
-  var capa1 = L.tileLayer.wms("http://vmap0.tiles.osgeo.org/wms/vmap0", {
-    layers: "basic",
-    format: "image/jpeg",
-    transparent: true,
-    attribution: "OsGEO",
-  });
-  capa1.addTo(map);
-  var capa2 = L.tileLayer.wms("http://www.ign.es/wms-inspire/pnoa-ma", {
-    layers: "OI.OrthoimageCoverage",
-    format: "image/jpeg",
-    transparent: true,
-    attribution: "PNOA",
-  });
-  var capa3 = L.tileLayer.wms(
-    "https://servicios.idee.es/wms-inspire/transportes?TN.RoadTransportNetwork.RoadLink",
-    {
-      layers: "TN.RoadTransportNetwork.RoadLink",
-      format: "image/png",
-      transparent: true,
-      attribution: "Carreteras",
-    }
-  );
-  var capa4 = L.Geoserver.wms(
-    "http://localhost:8080/geoserver/WebGIS/wms", // Change this URL to the one you have in your local GeoServer
-    {
-      layers: "carretera_acceso",
-      format: "image/png",
-      transparent: true,
-      attribution: "Acceso",
-    }
-  );
-  capa4.addTo(map);
-  var overlays = { Carreteras: capa3, Acceso: capa4 };
-  var baseLayers = { OSGEO: capa1, PNOA: capa2 };
-  L.control.layers(baseLayers, overlays).addTo(map);
+  [40.40562217022866, -3.834734211019332],
+  17
+);
+var capa1 = L.tileLayer.wms("http://vmap0.tiles.osgeo.org/wms/vmap0", {
+  layers: "basic",
+  format: "image/jpeg",
+  transparent: true,
+  attribution: "OsGEO",
+});
+capa1.addTo(map);
+var capa2 = L.tileLayer.wms("http://www.ign.es/wms-inspire/pnoa-ma", {
+  layers: "OI.OrthoimageCoverage",
+  format: "image/jpeg",
+  transparent: true,
+  attribution: "PNOA",
+});
+var capa3 = L.Geoserver.wms("http://localhost:8080/geoserver/SIGWeb/wms", {
+  layers: "SIGWeb:parqueaderos",
+  format: "image/png",
+  transparent: true,
+  attribution: "Parqueaderos",
+});
+capa3.addTo(map);
+var capa4 = L.Geoserver.wms("http://localhost:8080/geoserver/SIGWeb/wms", {
+  layers: "SIGWeb:carretera_acceso",
+  styles: "SIGWeb:accesos",
+  format: "image/png",
+  transparent: true,
+  attribution: "Acceso",
+});
+capa4.addTo(map);
+var capa5 = L.Geoserver.wms("http://localhost:8080/geoserver/SIGWeb/wms", {
+  layers: "SIGWeb:edificios",
+  styles: "SIGWeb:edificios",
+  format: "image/png",
+  transparent: true,
+  attribution: "Edificios",
+});
+capa5.addTo(map);
+var capa6 = L.Geoserver.wms("http://localhost:8080/geoserver/SIGWeb/wms", {
+  layers: "SIGWeb:canchas",
+  styles: "SIGWeb:canchas",
+  format: "image/png",
+  transparent: true,
+  attribution: "Canchas",
+});
+capa6.addTo(map);
+var capa7 = L.Geoserver.wms("http://localhost:8080/geoserver/SIGWeb/wms", {
+  layers: "SIGWeb:paradas",
+  styles: "SIGWeb:paradas",
+  format: "image/png",
+  transparent: true,
+  attribution: "Paradas",
+});
+capa7.addTo(map);
+// var overlays = { Carreteras: capa3, Acceso: capa4, Edificios: capa5 };
+var overlays = {
+  Parqueaderos: capa3,
+  Acceso: capa4,
+  Edificios: capa5,
+  Canchas: capa6,
+  Paradas: capa7,
+};
+var baseLayers = { OSGEO: capa1, PNOA: capa2 };
+L.control.layers(baseLayers, overlays).addTo(map);
